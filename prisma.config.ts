caa36922e6716317796c423ca9853b9b94a81687
Sameer Patel <samxpatel2@gmail.com>
2026-06-12 03:19:33 +0530

    prisma files update

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrate: {
-    schema: "prisma/schema.prisma",
-  },
   datasource: {
     url: process.env.DATABASE_URL!,
   },
